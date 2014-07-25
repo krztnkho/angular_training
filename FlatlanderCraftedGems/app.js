c5756f5c37fb4d22c300ff76c8d17438fd9e4622
Kristine Kho <Krztnkho@gmail.com>
2014-07-25 14:26:50 +0800

    lessons on nng show, hide and repeat.

--- FlatlanderCraftedGems/app.js
+++ FlatlanderCraftedGems/app.js
@@ -1,1 +1,24 @@
-var app = angular.module ( 'store' , [] );
+( function () {
+
+	var app = angular.module ( 'store' , [] );
+
+	app.controller( 'StoreController', function () {
+
+		this.products = gems;
+
+	} );
+
+	var gems = [ {
+		name        : 'Dodecahedron',
+		price       : 2.95,
+		description : '. . .',
+		canPurchase : false,
+		soldOut     : false
+	}, {
+		name        : 'Pentagonal Gem',
+		price       : 5.95,
+		description : '. . .',
+		canPurchase : false,
+		soldOut     : false
+	} ];
+} ) ();
