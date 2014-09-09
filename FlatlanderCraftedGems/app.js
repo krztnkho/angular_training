5f0876f88768e4ec3de8ba6d7174597de6d58d02
Kristine Kho <Krztnkho@gmail.com>
2014-09-09 09:51:42 +0800

    Refactor: Added Controllers.

--- FlatlanderCraftedGems/app.js
+++ FlatlanderCraftedGems/app.js
@@ -14,7 +14,16 @@
 		description : 'the dodecahedron gem is...',
 		canPurchase : false,
 		soldOut     : false,
-		image       : 'images/dodecahedron.png'
+		image       : 'images/dodecahedron.png',
+		reviews     : [ {
+			stars  : 5,
+			body   : 'I love this product!',
+			author : 'joe@thomas.com'
+		}, {
+			stars  : 1,
+			body   : 'This product sucks',
+			author : 'tim@hater.com'
+		} ]
 
 	}, {
 		name        : 'Pentagonal Gem',
@@ -22,6 +31,39 @@
 		description : 'the pentagonal gem is ...',
 		canPurchase : false,
 		soldOut     : false,
-		image       : 'images/pentagonal.png'
+		image       : 'images/pentagonal.png',
+		reviews     : [ {
+			stars  : 4,
+			body   : 'I love this product!',
+			author : 'joe@thomas.com'
+		}, {
+			stars  : 2,
+			body   : 'This product sucks',
+			author : 'tim@hater.com'
+		} ]
 	} ];
-} ) ();
+
+	app.controller( 'ReviewController' , function () {
+		this.review = {};
+
+		this.addReview = function ( product ){
+			product.reviews.push( this.review );
+			this.review = {};
+		};
+
+	} );
+
+	app.controller( 'PanelController' , function () {
+
+		this.tab = 1;
+
+		this.selectTab = function ( setTab ) {
+			this.tab = setTab;
+		};
+
+		this.isSelected = function ( checkTab ) {
+			return checkTab === this.tab;
+		};
+	} );
+
+} ) ( );
